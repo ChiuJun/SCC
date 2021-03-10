f9002d9d8315c9513066b14b6ecd6257cddf88f7
Chiu <lj.chiu@nit.edu.cn>
2021-03-10 17:38:12 +0800

    完成驱动器

--- scc.c
+++ scc.c
@@ -1,14 +1,19 @@
-//
-// Created by LJChi on 2021/3/8.
-//
+/*
+ * Created by LJChi on 2021/3/8.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <unistd.h>
 #include "scc.h"
 
-struct option Option;
-static char **cmd;
+static struct option Option;
+static char **commands;
+static List PPFiles;
+static List ASMFiles;
+static List OBJFIles;
 
 static void ShowHelp(void) {
     char *msg = {
@@ -42,7 +47,9 @@ static void *Alloc(size_t size) {
     return ptr;
 }
 
-//在 @list 尾部添加一个List节点，值为 @str;
+/*
+ * 在 @list 尾部添加一个List节点，值为 @str;
+ */
 static List ListAppend(List list, char *str) {
     List *tail = &list;
     List ptr = list;
@@ -57,86 +64,341 @@ static List ListAppend(List list, char *str) {
     return list;
 }
 
-static List ListCombine(List from, List to){
-    List *tail=&to;
-    List ptr=to;
-    while(ptr!=NULL){
-        tail=&(ptr->next);
-        ptr=ptr->next;
+static List ListCombine(List from, List to) {
+    List *tail = &to;
+    List ptr = to;
+    if (from == NULL)
+        return to;
+    while (ptr != NULL) {
+        tail = &(ptr->next);
+        ptr = ptr->next;
     }
-    *tail=from;
+    *tail = from;
 
     return to;
 }
 
-static int GetListLen(List list){
-    int cnt=0;
-    while(list!=NULL){
+static int GetListLen(List list) {
+    int cnt = 0;
+    while (list != NULL) {
         cnt++;
-        list=list->next;
+        list = list->next;
     }
 
     return cnt;
 }
 
-static int GetFileType(char *filename){
+static char *GenFileName(char *filename, char *extname) {
+    char *result;
+    int len, dot_p;
 
+    if (extname == NULL)
+        return filename;
+
+    len = strlen(filename);
+    dot_p = len - 1;
+    while (filename[dot_p] != '.' && dot_p != -1) {
+        dot_p--;
+    }
+    if (dot_p == -1) dot_p = len;
+    len = strlen(extname);
+    result = Alloc((dot_p + len + 1) * sizeof(char));
+    strncpy(result, filename, dot_p);
+    strcat(result, extname);
+
+    return result;
 }
 
-static void AddFile(char *filename){
+/*
+ * TODO: 暂时不支持.so文件
+ */
+static int GetFileType(char *filename) {
+    int idx, len_ext;
+    int len_file = strlen(filename);
+    for (idx = C_FILE; idx <= LIB_FILE; ++idx) {
+        len_ext = strlen(ExtNames[idx]);
+        if (!strncmp(ExtNames[idx], &filename[len_file - len_ext], len_ext))
+            return idx;
+    }
 
+    return -1;
 }
 
-static void RemoveFile(void){
+static void AddFile(char *filename) {
+    int type = GetFileType(filename);
+    if (type == -1) {
+        fprintf(stderr, "%s: Unrecognized file\n", filename);
+        return;
+    }
 
+    if (access(filename, R_OK)) {
+        fprintf(stderr, "%s: No such file\n", filename);
+        return;
+    }
+    switch (type) {
+        case C_FILE:
+            Option.c_files = ListAppend(Option.c_files, filename);
+            break;
+        case PP_FILE:
+            Option.p_files = ListAppend(Option.p_files, filename);
+            break;
+        case ASM_FILE:
+            Option.a_files = ListAppend(Option.a_files, filename);
+            break;
+        case OBJ_FILE:
+            Option.o_files = ListAppend(Option.o_files, filename);
+            break;
+        case LIB_FILE:
+            Option.l_files = ListAppend(Option.l_files, filename);
+            break;
+    }
 }
 
-static void ParseCmdLine(int argc,char *argv[]){
-    int i=0;
-    for (i = 0; i <argc; ++i) {
-        if(!strcmp(argv[i],"-E")){
-            Option.output_file_type=PP_FILE;
-        }else if(!strcmp(argv[i],"-S")){
-            Option.output_file_type=ASM_FILE;
-        }else if(!strcmp(argv[i],"-c")){
-            Option.output_file_type=OBJ_FILE;
-        }else if(!strcmp(argv[i],"-o")){
-            Option.out_file_name=argv[++i];
-        }else if(!strncmp(argv[i],"-I",2)||
-                !strncmp(argv[i],"-D",2)||
-                !strncmp(argv[i],"-U",2)){
-            Option.p_flags=ListAppend(Option.p_flags,argv[i]);
-        }else if(!strcmp(argv[i],"-h")){
+static void CleanFiles(void) {
+    List ptr;
+    if (Option.output_file_type > PP_FILE)
+        for (ptr = PPFiles; ptr != NULL; ptr = ptr->next)
+            remove(ptr->str);
+    if (Option.output_file_type > ASM_FILE)
+        for (ptr = ASMFiles; ptr != NULL; ptr = ptr->next)
+            remove(ptr->str);
+    if (Option.output_file_type > OBJ_FILE)
+        for (ptr = OBJFIles; ptr != NULL; ptr = ptr->next)
+            remove(ptr->str);
+}
+
+static void ParseCmdLine(int argc, char *argv[]) {
+    int i = 0;
+    for (i = 0; i < argc; ++i) {
+        if (!strcmp(argv[i], "-E")) {
+            Option.output_file_type = PP_FILE;
+        } else if (!strcmp(argv[i], "-S")) {
+            Option.output_file_type = ASM_FILE;
+        } else if (!strcmp(argv[i], "-c")) {
+            Option.output_file_type = OBJ_FILE;
+        } else if (!strcmp(argv[i], "-o")) {
+            Option.out_file_name = argv[++i];
+        } else if (!strncmp(argv[i], "-I", 2) ||
+                   !strncmp(argv[i], "-D", 2) ||
+                   !strncmp(argv[i], "-U", 2)) {
+            Option.p_flags = ListAppend(Option.p_flags, argv[i]);
+        } else if (!strcmp(argv[i], "-h")) {
             ShowHelp();
             exit(0);
-        }else if(!strcmp(argv[i],"-v")){
-            Option.verbose=1;
-        }else if(!strcmp(argv[i],"--dump-ast")||
-                !strcmp(argv[i],"--dump-IR")){
-            Option.c_flags=ListAppend(Option.c_flags,argv[i]);
-        }else if(!strncmp(argv[i],"-Wa,",4)){
-            Option.a_flags=ListAppend(Option.a_flags,argv[i]);
-        }else if(!strncmp(argv[i],"-Wl,",4)){
-            Option.l_flags=ListAppend(Option.l_flags,argv[i]);
-        }else if(argv[i][0]=='-'){//所有其他选项都传递给连接器
-            Option.l_input=ListAppend(Option.l_input,argv[i]);
-        }else{
+        } else if (!strcmp(argv[i], "-v")) {
+            Option.verbose = 1;
+        } else if (!strcmp(argv[i], "--dump-ast") ||
+                   !strcmp(argv[i], "--dump-IR")) {
+            Option.c_flags = ListAppend(Option.c_flags, argv[i]);
+        } else if (!strncmp(argv[i], "-Wa,", 4)) {
+            Option.a_flags = ListAppend(Option.a_flags, argv[i]);
+        } else if (!strncmp(argv[i], "-Wl,", 4)) {
+            Option.l_flags = ListAppend(Option.l_flags, argv[i]);
+        } else if (argv[i][0] == '-') {/*所有其他选项都传递给连接器*/
+            Option.l_flags = ListAppend(Option.l_flags, argv[i]);
+        } else {
             AddFile(argv[i]);
         }
     }
 }
 
+static int ExecuteCommand(char **cmd) {
+    int pid, ret, status;
+
+    pid = fork();
+    if (pid == -1) {
+        fprintf(stderr, "fork error,no more processes\n");
+        return 100;
+    } else if (pid == 0) {/*子进程*/
+        execv(cmd[0], cmd);
+        perror(cmd[0]);
+        fflush(stdout);
+        exit(100);
+    }
+    /*父进程*/
+    ret = waitpid(pid, &status, 0);
+    status = ret == -1 ? ret : status;
+    if (status & 0xff) {
+        fprintf(stderr, "fatal error in %s\n", cmd[0]);
+        status |= 0x100;
+    }
+    return (status >> 8) & 0xff;
+}
+
+static void PrintCommand(void) {
+    if (commands[0] == NULL) return;
+
+    int i;
+    printf("%s", commands[0]);
+    for (i = 1; commands[i] != NULL; ++i) {
+        printf(" %s", commands[i]);
+    }
+    printf("\n");
+}
+
+static char **GenCommand(char **cmd, List flags, List in_files, List out_files) {
+    int i, j;
+    List listArr[3], tmpList;
+    listArr[0] = flags;/*$1*/
+    listArr[1] = in_files;/*$2*/
+    listArr[2] = out_files;/*$3*/
+
+    for (i = j = 0; cmd[i] != NULL; ++i) {
+        if (strchr(cmd[i], '$') != NULL) {
+            tmpList = listArr[(cmd[i][1] - '0') - 1];
+            while (tmpList != NULL) {
+                commands[j++] = tmpList->str;
+                tmpList = tmpList->next;
+            }
+        } else {
+            commands[j++] = cmd[i];
+        }
+    }
+    commands[j] = NULL;
+
+    if (Option.verbose == 1)
+        PrintCommand();
+
+    return commands;
+}
+
+static List ParseOption(char *opt) {
+    List split_option = NULL;
+    int len = strlen(opt);
+    char *str = (char *) Alloc(len * sizeof(char));
+    char *tmp;
+    strcpy(str, opt);
+    tmp = strtok(str, ",");
+    while (tmp != NULL) {
+        split_option = ListAppend(split_option, tmp);
+        tmp = strtok(NULL, ",");
+    }
+
+    return split_option;
+}
+
+static int InvokeProgram(int filetype) {
+    int status = 0;
+    List ptr, in_files, out_files;
+    char *filename, **cmd;
+
+    switch (filetype) {
+        case PP_FILE:
+            for (ptr = Option.c_files; ptr != NULL; ptr = ptr->next) {
+                if (Option.output_file_type == PP_FILE && Option.out_file_name) {
+                    /*Option.output_file_type==PP_FILE时
+                     * GetListLen(Option.c_files)==1
+                     * */
+                    filename = Option.out_file_name;
+                } else {
+                    filename = GenFileName(ptr->str, ExtNames[PP_FILE]);
+                }
+                in_files = ListAppend(NULL, ptr->str);
+                out_files = ListAppend(NULL, filename);
+                cmd = GenCommand(CPPCmd, Option.p_flags, in_files, out_files);
+                status = ExecuteCommand(cmd);
+                PPFiles = ListAppend(PPFiles, filename);
+            }
+            Option.p_files = ListCombine(PPFiles, Option.p_files);
+            break;
+        case ASM_FILE:
+            for (ptr = Option.p_files; ptr != NULL; ptr = ptr->next) {
+                if (Option.output_file_type == ASM_FILE && Option.out_file_name) {
+                    filename = Option.out_file_name;
+                } else {
+                    filename = GenFileName(ptr->str, ExtNames[ASM_FILE]);
+                }
+                in_files = ListAppend(NULL, ptr->str);
+                out_files = ListAppend(NULL, filename);
+                cmd = GenCommand(CCCmd, Option.c_flags, in_files, out_files);
+                status = ExecuteCommand(cmd);
+                ASMFiles = ListAppend(ASMFiles, filename);
+            }
+            Option.a_files = ListCombine(ASMFiles, Option.a_files);
+            break;
+        case OBJ_FILE:
+            for (ptr = Option.a_flags, Option.a_flags = NULL; ptr != NULL; ptr = ptr->next) {
+                Option.a_flags = ListCombine(ParseOption((ptr->str) + 4), Option.a_flags);
+            }
+            for (ptr = Option.a_files; ptr != NULL; ptr = ptr->next) {
+                if (Option.output_file_type == OBJ_FILE && Option.out_file_name) {
+                    filename = Option.out_file_name;
+                } else {
+                    filename = GenFileName(ptr->str, ExtNames[OBJ_FILE]);
+                }
+                in_files = ListAppend(NULL, ptr->str);
+                out_files = ListAppend(NULL, filename);
+                cmd = GenCommand(ASCmd, Option.a_flags, in_files, out_files);
+                status = ExecuteCommand(cmd);
+                OBJFIles = ListAppend(OBJFIles, filename);
+            }
+            Option.o_files = ListCombine(OBJFIles, Option.o_files);
+            break;
+        case LIB_FILE:
+            /*TODO:暂时不支持生成.so*/
+            return 0;
+            break;
+        case EXE_FILE:
+            if (Option.out_file_name == NULL) {
+                Option.out_file_name = GenFileName("a.out", ExtNames[EXE_FILE]);
+            }
+            for (ptr = Option.l_flags, Option.l_flags = NULL; ptr != NULL; ptr = ptr->next) {
+                Option.l_flags = ListCombine(ParseOption(ptr->str), Option.l_flags);
+            }
+            cmd = GenCommand(LDCmd, Option.l_flags, Option.o_files, ListAppend(NULL, Option.out_file_name));
+            status = ExecuteCommand(cmd);
+            break;
+    }
+
+    return status;
+}
+
+static void TestScc(void) {
+    assert(0 == GetFileType("test.c"));
+    assert(1 == GetFileType("test.i"));
+    assert(2 == GetFileType("test.s"));
+    assert(3 == GetFileType("test.o"));
+    assert(4 == GetFileType("test.so"));
+    assert(-1 == GetFileType("test.a"));
+    assert(!strcmp(GenFileName("test.c", ".o"), "test.o"));
+    assert(!strcmp(GenFileName("test.test.c", ".o"), "test.test.o"));
+    assert(!strcmp(GenFileName("test", ".o"), "test.o"));
+    assert(!strcmp(GenFileName("test.cpp", ".o"), "test.o"));
+    assert(!strcmp(GenFileName("test.c", NULL), "test.c"));
+    assert(!strcmp(GenFileName("test.c", 0), "test.c"));
+    assert(!strcmp(GenFileName("test.c", ".so"), "test.so"));
+    assert(!strcmp(GenFileName("test.c", "."), "test."));
+    exit(0);
+}
+
 int main(int argc, char *argv[]) {
+    int filetype;
     if (argc < 2) {
         ShowHelp();
-        exit(0);
+        return 0;
     }
-    
-    //TODO:为什么加60
-    cmd = (char **)Alloc((argc+60)*(sizeof(char *)));
-    cmd[0] = NULL;
-    Option.output_file_type=EXE_FILE;
-    ParseCmdLine(--argc,++argv);
+
+    commands = (char **) Alloc((argc + 100) * (sizeof(char *)));
+    commands[0] = NULL;
+    Option.output_file_type = EXE_FILE;
+    ParseCmdLine(--argc, ++argv);
+    if (Option.out_file_name && Option.output_file_type != EXE_FILE &&
+        (GetListLen(Option.c_files) > 1 ||
+         GetListLen(Option.p_files) > 1 ||
+         GetListLen(Option.a_files) > 1)) {
+        fprintf(stderr, "cannot specify -o with -c, -S or -E with multiple files\n");
+        return -1;
+    }
+
+    for (filetype = PP_FILE; filetype <= Option.output_file_type; ++filetype) {
+        if (InvokeProgram(filetype)) {
+            CleanFiles();
+            fprintf(stderr, "scc invoke command error:\n");
+            PrintCommand();
+            return -1;
+        }
+    }
+    CleanFiles();
 
     return 0;
 }
--- scc.h
+++ scc.h
@@ -1,17 +1,20 @@
-//
-// Created by LJChi on 2021/3/8.
-//
+/*
+ * Created by LJChi on 2021/3/8.
+ */
 
 #ifndef SCC_SCC_H
 #define SCC_SCC_H
 
-// PP_FILE:PreprocessFile
-// TODO:gcc貌似只能生成动态链接库，所以ExtNames只写了.so
+#define SCCDIR "/home/jiabi/ucc/bin/"
+
+/*
+ * PP_FILE:PreprocessFile
+ */
 enum {
     C_FILE, PP_FILE, ASM_FILE, OBJ_FILE, LIB_FILE, EXE_FILE
 };
 
-char *ExtNames[] = {".c", ".i", ".s", ".o", ".a;.so", NULL};
+char *ExtNames[] = {".c", ".i", ".s", ".o", ".so", NULL};
 
 typedef struct list {
     char *str;
@@ -19,19 +22,56 @@ typedef struct list {
 } *List;
 
 struct option {
-    List p_flags;//preprocessor
-    List c_flags;//compiler
-    List a_flags;//assembler
-    List l_flags;//linker
-    List c_files;//.c
-    List p_files;//.i
-    List a_files;//.s
-    List o_files;//.o
-    List l_files;//.a .lib
-    List l_input;//input for linker
-    int verbose;//verbose mode flag
-    int output_file_type;//output file type
+    List p_flags;/*preprocessor*/
+    List c_flags;/*compiler*/
+    List a_flags;/*assembler*/
+    List l_flags;/*linker*/
+    List c_files;/*.c*/
+    List p_files;/*.i*/
+    List a_files;/*.s*/
+    List o_files;/*.o*/
+    List l_files;/*.a .so*/
+    int verbose;/*verbose mode flag*/
+    int output_file_type;
     char *out_file_name;
 };
 
-#endif //SCC_SCC_H
+/**
+	ucc -E -v hello.c -I../ -DREAL=double -o hello.ii
+		For $1/$2/$3	,see GenCommand() for detail.
+		$1		-I../ -DREAL=double,	command-line options
+		$2		hello.c,		input file
+		$3		hello.ii,		output file
+*/
+char *CPPCmd[] =
+        {
+                "/usr/bin/gcc", "-m32", "-U__GNUC__", "-D_POSIX_SOURCE", "-D__STRICT_ANSI__",
+                "-Dunix", "-Di386", "-Dlinux", "-D__unix__", "-D__i386__", "-D__linux__",
+                "-D__signed__=signed", "-D_UCC", "-I" SCCDIR "include", "$1", "-E", "$2", "-o", "$3", 0
+        };
+
+
+/**
+	ucc -S -v hello.c --dump-ast -o hello.asm
+		------->
+	/home/jiabi/ucc/bin/ucl -o hello.asm --dump-ast hello.i
+		$1	--dump-ast,	some command-line options
+		$2	hello.i,	input file
+		$3	hello.asm,	output file
+*/
+char *CCCmd[] =
+        {
+                SCCDIR "ucl", "-o", "$3", "$1", "$2", 0
+        };
+
+char *ASCmd[] =
+        {
+                "/usr/bin/gcc", "-m32", "-c", "-o", "$3", "$1", "$2", 0
+        };
+
+char *LDCmd[] =
+        {
+                "/usr/bin/gcc", "-m32", "-o", "$3", "$1", "$2", SCCDIR "assert.o", "-lc", "-lm", 0
+        };
+
+#endif
