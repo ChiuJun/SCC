1271a3fc20a5835d0f9b4779971936ba99fe8fb0
Chiu <lj.chiu@nit.edu.cn>
2021-04-13 14:12:18 +0800

    修复逻辑错误

--- cc/alloc.c
+++ cc/alloc.c
@@ -19,33 +19,50 @@ void InitHeap(Heap hp) {
 /*
  * 从当前Heap的最后一个memory_block或者FreeBlocks寻找足够空间并返回
  * 如果空间不够则使用malloc申请新的内存
- * TODO:这种分配策略可能会产生较多内存碎片
  * */
 void *HeapAllocate(Heap hp, size_t size) {
     size = ALIGN(size, sizeof(union align_util));
-    struct memory_block *blk = hp->lastBlockPtr;
+    struct memory_block *blk = hp->headBlock.next;
+    struct memory_block *preBlk = NULL;
 
-    while (size > (blk->end - blk->avail)) {
-        if ((blk->next = FreeBlocks) != NULL) {
-            /*首选到空闲内存块中寻找可用内存块*/
-            FreeBlocks = FreeBlocks->next;
+    while (blk != NULL) {
+        if (size > (blk->end - blk->avail)) {
             blk = blk->next;
         } else {
-            /*无空闲内存块则申请新的内存块*/
-            size_t blk_size = size + MEMORY_BLOCK_SIZE + sizeof(struct memory_block);
-            blk->next = (struct memory_block *) malloc(blk_size);
-            blk = blk->next;
-            if (blk == NULL) {
-                Fatal("Memory exhausted");
-            }
-            blk->end = (char *) blk + blk_size;
+            break;
         }
-        blk->next = NULL;
+    }
+    if (blk == NULL) {/*heap上没有合适的内存块则到FreeBlocks中寻找*/
+        preBlk = NULL;
+        blk = FreeBlocks;
+        while (blk != NULL) {
+            if (size > (blk->end - blk->avail)) {
+                preBlk = blk;
+                blk = blk->next;
+            } else {
+                if(preBlk == NULL) FreeBlocks = blk->next;
+                else preBlk->next = blk->next;
+                blk->next = NULL;
+                hp->lastBlockPtr->next = blk;
+                hp->lastBlockPtr = blk;
+                break;
+            }
+        }
+    }
+    if (blk == NULL) {/*无空闲内存块则申请新的内存块*/
+        size_t blk_size = size + MEMORY_BLOCK_SIZE + sizeof(struct memory_block);
+        blk = (struct memory_block *) malloc(blk_size);
+        if (blk == NULL) {
+            Fatal("Memory exhausted");
+        }
+        blk->end = (char *) blk + blk_size;
         blk->begin = blk->avail = (char *) (blk + 1);
+        blk->next = NULL;
+        hp->lastBlockPtr->next = blk;
         hp->lastBlockPtr = blk;
     }
-    blk->avail += size;
 
+    blk->avail += size;
     return blk->avail - size;
 }
 
@@ -54,7 +71,12 @@ void *HeapAllocate(Heap hp, size_t size) {
  * 并不真正回收空间，只是挂载至FreeBlocks
  * */
 void FreeHeap(Heap hp) {
+    struct memory_block *blk = &hp->headBlock.next;
+    while (blk != NULL) {
+        blk->avail = blk->begin;
+        blk = blk->next;
+    }
     hp->lastBlockPtr->next = FreeBlocks;
-    FreeBlocks->next = hp->headBlock.next;
+    FreeBlocks = hp->headBlock.next;
     InitHeap(hp);
 }
